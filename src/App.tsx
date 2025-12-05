dec9965b71be2c2866492de0513694bf191162b0
Meegu80 <aceman80@naver.com>
2025-12-05 17:16:40 +0900

    Revert "Movie 초기화 필요한 파일들 생성 및 App 라우터 완료"
    This reverts commit 1d640bf9

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import {BrowserRouter, Route, Routes} from "react-router";
-import Detail from "./pages/Detail.tsx";
-import Home from "./pages/Home.tsx";
+import Detail from "./components/Detail.tsx";
+import Home from "./components/Home.tsx";
 import {Reset} from "styled-reset";
-import Search from "./pages/Search.tsx";
 
 function App() {
+
     return (
         <BrowserRouter>
             <Reset/>
             <Routes>
                 <Route path="/detail/:id" element={<Detail/>}/>
-                <Route path="/search" element={<Search/>}/>
                 <Route path="/" element={<Home/>}/>
             </Routes>
         </BrowserRouter>
